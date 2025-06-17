fa0480fc0556da9076ef89f5e2909dcd348db014
Chenda001 <chendaketer@gmail.com>
2025-06-17 16:42:51 +0300

    Update views.py

--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
-from django.contrib import messages
+from django.contrib import messages 
 from .models import Users
 from django.contrib.auth.decorators import login_required
 
