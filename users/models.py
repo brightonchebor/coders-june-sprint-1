3cfb7f2caf35fcd23386cb0f934db6cd3ce4decb
Kat-tech15 <kelvikatwai@gmail.com>
2025-06-17 13:48:32 +0300

    updates

--- users/models.py
+++ users/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Staff(models.Model):
+    username = models.CharField(max_length=150, unique=True)
+    email = models.EmailField(unique=True)
+    department = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    position = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.username
+
+    
